dbb6017f1af168d192fd30bc1647aa4b96d9581c
ptodvy <ptodvy@gmail.com>
2024-02-20 15:32:04 +0900

    etc - set project

---
+++ ios-ux-preview/ios_ux_previewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_ux_previewApp.swift
+//  ios-ux-preview
+//
+//  Created by bruno on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_ux_previewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
